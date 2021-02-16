8664b954388d11d1d83638a31d9336547dcbac94
Garrett Rubinstein <garrub.bergen@gmail.com>
2021-02-15 18:20:57 -0600

    play audio from multiple sources and handle time updates

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,6 +4,11 @@ const App = () => {
   const [paths, setPaths] = useState([]);
   const audio1 = useRef();
   const audio2 = useRef();
+  const [ready1, setReady1] = useState(false);
+  const [ready2, setReady2] = useState(false);
+  var prevTime1 = 0;
+  var prevTime2 = 0;
+
 
 
   useEffect(() => {
@@ -21,15 +26,24 @@ const App = () => {
     if (paths.length === 0) {
       return;
     }
-    setInterval(() => {
-      console.log(audio1.current.currentTime);
-    }, 1000);
   }, [paths]);
+
+  const handleTimeUpdate = (source) => {
+
+  }
+
+
 
   return (
     <div>
-      <audio ref={audio1} src="assets/music/oopsIDidItAgain.mp3" preload="auto" type="audio/mpeg" controls>Your browser does not support the audio tag</audio>
-      <audio ref={audio2} src="assets/music/babyOneMoreTime.mp3" preload="auto" type="audio/mpeg" controls>Your browser does not support the audio tag</audio>
+      <audio ref={audio1} src="assets/music/oopsIDidItAgain.mp3" preload="auto" type="audio/mpeg" controls onCanPlayThrough={() => setReady1(true)} onTimeUpdate={() => {
+        console.log(potato);
+        potato = audio1.current.currentTime;
+      }}>
+        Your browser does not support the audio tag
+      </audio>
+      <audio ref={audio2} src="assets/music/babyOneMoreTime.mp3" preload="auto" type="audio/mpeg" controls onCanPlayThrough={() => setReady2(true)}>Your browser does not support the audio tag</audio>
+      <button disabled={!ready1 || !ready2 || (paths.length === 0)} onClick={() => audio1.current.play()}>Play!</button>
     </div>
   );
 };
