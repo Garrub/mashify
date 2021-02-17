e5b3c3e419886376027d91f04743ad0fa7b5f22f
Garrett Rubinstein <garrub.bergen@gmail.com>
2021-02-17 12:27:56 -0600

    added songlist structure

---
+++ client/lib/songlist.js
@@ -0,0 +1,24 @@
+const songlist = {
+  allStar: {
+    id: 'allStar',
+    track: 'all star',
+    artist: 'smash mouth'
+  },
+  babyOneMoreTime: {
+    id: 'babyOneMoreTime',
+    track: 'baby one more time',
+    artist: 'britney spears'
+  },
+  gangnamStyle: {
+    id: 'gangnamStyle',
+    track: 'gangnam style',
+    artist: 'psy'
+  },
+  oopsIDidItAgain: {
+    id: 'oopsIDidItAgain',
+    track: 'oops i did it again',
+    artist: 'britney spears'
+  }
+}
+
+export default songlist;
